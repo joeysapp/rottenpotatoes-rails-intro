ecfcb8df388257d21a716ccc7a54a7d886bf9569
Joseph Sapp <sapp.joey@tamu.edu>
2018-02-15 15:52:01 -0600

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,21 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def hiliter(t_field)
+  	if (params[:sort].to_s == t_field)
+  		return 'hilite'
+  	else
+  		return nil
+  	end
+  end
+
+  def sorter(t_movie)
+  	if (params[:sort].to_s == 'release')
+  		return t_movie.release_date.to_s
+  	elsif (params[:sort].to_s == 'title')
+  		return t_movie.title.to_s
+  	end
+  end
+  
 end
